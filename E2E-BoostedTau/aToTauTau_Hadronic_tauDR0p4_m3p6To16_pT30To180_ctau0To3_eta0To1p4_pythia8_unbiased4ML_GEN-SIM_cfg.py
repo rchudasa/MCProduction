4ba08b885d0b17a6776a8816a3b49a630efbc67c
Ruchi <ruchi.chudasama@cern.ch>
2023-02-07 05:06:02 -0600

    unnecessary files deleted

--- E2E-BoostedTau/aToTauTau_Hadronic_tauDR0p4_m3p6To16_pT30To180_ctau0To3_eta0To1p4_pythia8_unbiased4ML_GEN-SIM_cfg.py
+++ E2E-BoostedTau/aToTauTau_Hadronic_tauDR0p4_m3p6To16_pT30To180_ctau0To3_eta0To1p4_pythia8_unbiased4ML_GEN-SIM_cfg.py
@@ -2,12 +2,12 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: SingleNuE10_cfi.py --fileout file:GEN.root --mc --eventcontent RAWSIM --datatier GEN --conditions 106X_upgrade2018_realistic_v15_L1v1 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN --geometry DB:Extended --era Run2_2018 --python_filename GEN_2018_cfg.py -n 10 --runUnscheduled --no_exec
+# with command line options: SingleNuE10_cfi.py --python_filename aToTauTau_GEN-SIM_cfg.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:aToTauTau_GEN-SIM.root --conditions 106X_upgrade2018_realistic_v4 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --mc -n 1000
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 
-process = cms.Process('GEN',Run2_2018)
+process = cms.Process('SIM',Run2_2018)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -16,10 +16,12 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
 process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2018Collision_cfi')
 process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
@@ -38,8 +40,7 @@ process.genHToTauTauFilter = cms.EDFilter("GenHToTauTauFilter",
 )
 
 process.maxEvents = cms.untracked.PSet(
-    #input = cms.untracked.int32(100000)
-    input = cms.untracked.int32(50000)
+    input = cms.untracked.int32(3000000)
 )
 
 # Input source
@@ -51,7 +52,7 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True)
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('SingleNuE10_cfi.py nevts:10'),
+    annotation = cms.untracked.string('SingleNuE10_cfi.py nevts:1000'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -65,11 +66,11 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     compressionAlgorithm = cms.untracked.string('LZMA'),
     compressionLevel = cms.untracked.int32(1),
     dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('GEN'),
+        dataTier = cms.untracked.string('GEN-SIM'),
         filterName = cms.untracked.string('')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
-    fileName = cms.untracked.string('file:GEN_HToTauTau_m3p6To15p2_pT30To180_ctau0To3_unbiased.root'),
+    fileName = cms.untracked.string('file:aToTauTau_GEN-SIM.root'),
     outputCommands = process.RAWSIMEventContent.outputCommands,
     splitLevel = cms.untracked.int32(0)
 )
@@ -77,16 +78,17 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
 # Additional output definition
 
 # Other statements
+process.XMLFromDBSource.label = cms.string("Extended")
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v15_L1v1', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v4', '')
 
 process.generator = cms.EDFilter("Pythia8PtGunV3",
     PGunParameters = cms.PSet(
         AddAntiParticle = cms.bool(True),
         MaxCTau = cms.double(3.0),
         MaxEta = cms.double(1.4),
-        MaxMass = cms.double(15.2),
+        MaxMass = cms.double(16.0),
         MaxPhi = cms.double(3.14159265359),
         MaxPt = cms.double(180.0),
         MinCTau = cms.double(0.0),
@@ -100,38 +102,43 @@ process.generator = cms.EDFilter("Pythia8PtGunV3",
         parameterSets = cms.vstring('processParameters'),
         processParameters = cms.vstring(
             '25:onMode = off',
-            '25:onIfAny = 15',
+            '25:onIfMatch = 15 -15',
             '15:onMode = on',
-            '15:offIfAny = 11 13'
+            '15:offIfAny = 11 -11 13 -13'
         )
     ),
     Verbosity = cms.untracked.int32(0),
     firstRun = cms.untracked.uint32(1),
     maxEventsToPrint = cms.untracked.int32(1),
-    psethack = cms.string('H to tautau pTgun'),
+    psethack = cms.string('a to tautau pTgun'),
     pythiaHepMCVerbosity = cms.untracked.bool(True),
     pythiaPylistVerbosity = cms.untracked.int32(1)
 )
 
 # Path and EndPath definitions
-#process.generation_step = cms.Path(process.pgen)
 process.generation_step = cms.Path(process.pgen+process.genHToTauTauFilter)
+process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RAWSIMoutput_step)
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 # filter all path with the production filter sequence
 for path in process.paths:
 	getattr(process,path).insert(0, process.generator)
 
-#do not add changes to your config after this point (unless you know what you are doing)
-from FWCore.ParameterSet.Utilities import convertToUnscheduled
-process=convertToUnscheduled(process)
+# customisation of the process.
 
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
 
 # Customisation from command line
 
