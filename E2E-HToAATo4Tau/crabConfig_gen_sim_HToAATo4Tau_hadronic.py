ee24b0007dce438282844c130979336adebb041d
rchudasa <ruchi.chudasama@cern.ch>
2024-09-20 17:55:48 +0200

    gen sim run3 scripts added

---
+++ E2E-HToAATo4Tau/crabConfig_gen_sim_HToAATo4Tau_hadronic.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config
+config = config()
+# See parameter defintions here: https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile#CRAB_configuration_parameters
+Mass='14' # Mass of A is generally integer but put as string if need decimal.
+# Local job directory will be created in:
+config.General.requestName = 'GEN_SIM_HToAATo4Tau_M%s'%Mass
+config.General.workArea = 'crab_MC'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+# CMS cfg file goes here:
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'GEN_SIM_HToAATo4Tau_M%s_cfg.py'%Mass
+config.Data.outputPrimaryDataset = 'HToAATo4Tau_M%s_Run3_2023'%Mass
+
+#config.JobType.maxMemoryMB = 2800
+
+# Define units per job here:
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.eventsPerLumi=500
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 1100 # units: large number is given because HToaaTo4Tau has filters about 8 % eff
+NJOBS = 100
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.publication = True
+
+# Output files will be stored in config.Site.storageSite at directory:
+config.Data.outLFNDirBase = '/store/group/phys_diffraction/rchudasa/MCGeneration'
+config.Site.storageSite = 'T2_CH_CERN'
