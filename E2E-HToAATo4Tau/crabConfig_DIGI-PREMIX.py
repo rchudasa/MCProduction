614462f66cc2bed409a9adad7085dd1216fc59fc
rchudasa <ruchi.chudasama@cern.ch>
2024-12-06 13:57:32 +0100

    digi raw scripts

--- E2E-HToAATo4Tau/crabConfig_DIGI-PREMIX.py
+++ E2E-HToAATo4Tau/crabConfig_DIGI-PREMIX.py
@@ -1,8 +1,8 @@
 from CRABClient.UserUtilities import config
 config = config()
 #Mass = '14'#14
-#Mass = '3p7'#14
-Mass = 'WJets'
+Mass = '4'#14
+#Mass = 'WJets'
 
 inputProcess_ = {
 #'3p7': "/HToAATo4Tau_M3p7_Run3_2023/phys_diffraction-crab_GEN_SIM_HToAATo4Tau_M3p7-2b4ddcb85c0e2aa1bf748c03bd100e09/USER"
@@ -10,6 +10,10 @@ inputProcess_ = {
 '3p7': "/HToAATo4Tau_hadronic_tauDecay_M3p7_Run3_2023/phys_diffraction-crab_GEN_SIM_HToAATo4Tau_hadronic_tauDecay_M3p7-dcbefa18cc6f5cfd3c8d2c66024d0c0c/USER"
 , '14': "/HToAATo4Tau_hadronic_tauDecay_M14_Run3_2023/phys_diffraction-crab_GEN_SIM_HToAATo4Tau_hadronic_tauDecay_M14_v2-86d29732f27943385419c19318645e94/USER"
 ,'WJets':'WJets_digiraw.txt'
+,'3p7': "/HToAATo4Tau_hadronic_tauDecay_M3p7_Run3_2023/lpcml-crab_GEN_SIM_HToAATo4Tau_tauDecay_M3p7-dcbefa18cc6f5cfd3c8d2c66024d0c0c/USER"
+,'4': "/HToAATo4Tau_hadronic_tauDecay_M4_Run3_2023/lpcml-crab_GEN_SIM_HToAATo4Tau_tauDecay_M4-65fda359da575d58201444a7a8375d6b/USER"
+,'5': "/HToAATo4Tau_hadronic_tauDecay_M5_Run3_2023/lpcml-crab_GEN_SIM_HToAATo4Tau_tauDecay_M5-eb1606952c4e09f97648e0927768ea7f/USER"
+,'6': "/HToAATo4Tau_hadronic_tauDecay_M6_Run3_2023/lpcml-crab_GEN_SIM_HToAATo4Tau_tauDecay_M6-8c4e993c2f7de8c3cb712019877abf5a/USER"
 }.get(Mass, None)
 
 #config.section_('General')
@@ -20,21 +24,22 @@ config.General.transferLogs = True
 
 #config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'step2_DIGI-RAW-HLT_cfg.py'
+config.JobType.psetName = 'step2_DIGI-RAW-HLT_pileupFileList_cfg.py'
 config.JobType.maxMemoryMB = 4000
+config.JobType.numCores = 8
 
 config.Data.inputDBS = 'phys03'
 config.JobType.allowUndistributedCMSSW = True
-#config.Data.inputDataset = inputProcess_
+config.Data.inputDataset = inputProcess_
 
-config.Data.userInputFiles = open('%s'%inputProcess_).readlines()
+#config.Data.userInputFiles = open('%s'%inputProcess_).readlines()
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1 
-config.Data.outputPrimaryDataset = 'WtoLNu-2Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8' 
+#config.Data.outputPrimaryDataset = 'WtoLNu-2Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8' 
 
-config.Data.outLFNDirBase = '/store/group/lpcml/rchudasa/MCGenerationRun3'
 #config.Data.outLFNDirBase = '/store/group/phys_diffraction/rchudasa/MCGeneration'
 #config.Site.storageSite = 'T2_CH_CERN'
+config.Data.outLFNDirBase = '/store/group/lpcml/rchudasa/MCGenerationRun3'
 config.Site.storageSite = 'T3_US_FNALLPC'
 config.Data.publication = True 
 config.Data.outputDatasetTag = config.General.requestName
