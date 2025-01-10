7b98882981c11655c76d06a6a4ee85d3b23dace2
rchudasa <ruchi.chudasama@cern.ch>
2025-01-10 04:59:39 +0100

    crab config update for big production v2

--- E2E-HToAATo4Tau/crabConfig_gen_sim_HToAATo4Tau_hadronic.py
+++ E2E-HToAATo4Tau/crabConfig_gen_sim_HToAATo4Tau_hadronic.py
@@ -26,7 +26,7 @@ outputDataset_ = {
 }.get(Mass, None)
 
 config.General.requestName = 'GEN_SIM_HToAATo4Tau_tauDecay_M%s'%Mass
-config.General.workArea = 'crab_bigProduction'
+config.General.workArea = 'crab_newBigProduction'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
@@ -43,11 +43,27 @@ config.JobType.numCores = 8
 config.JobType.allowUndistributedCMSSW = True
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 5000 # units: large number is given because HToaaTo4Tau has filters about 60% eff
-NJOBS = 1000
+NJOBS = 1200
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
 
 # Output files will be stored in config.Site.storageSite at directory:
+config.Data.ignoreLocality = True
+config.Site.whitelist = [
+    'T2_AT_Vienna', 'T2_BE_IIHE', 'T2_BE_UCL', 'T2_BR_SPRACE', 'T2_BR_UERJ',
+    'T2_CH_CERN', 'T2_CN_Beijing', 'T2_DE_DESY', 'T2_DE_RWTH',
+    'T2_EE_Estonia', 'T2_ES_CIEMAT', 'T2_ES_IFCA', 'T2_FI_HIP',
+    'T2_FR_IPHC', 'T2_GR_Ioannina', 'T2_HU_Budapest', 'T2_IN_TIFR',
+    'T2_IT_Bari', 'T2_IT_Legnaro', 'T2_IT_Pisa', 'T2_IT_Rome',
+    'T2_KR_KISTI', 'T2_PK_NCP', 'T2_PL_Cyfronet',
+    'T2_PT_NCG_Lisbon', 'T2_RU_IHEP',
+    'T2_TR_METU', 'T2_TW_NCHC', 'T2_UA_KIPT',
+    'T2_UK_London_Brunel', 'T2_UK_London_IC', 'T2_UK_SGrid_Bristol',
+    'T2_UK_SGrid_RALPP', 'T2_US_Caltech', 'T2_US_Florida',
+    'T2_US_MIT', 'T2_US_Nebraska', 'T2_US_Purdue', 'T2_US_UCSD',
+    'T2_US_Vanderbilt', 'T2_US_Wisconsin'
+]
+
 config.Data.outLFNDirBase = '/store/group/lpcml/rchudasa/MCGenerationRun3'
 config.Site.storageSite = 'T3_US_FNALLPC'
 #config.Data.outLFNDirBase = '/store/group/phys_diffraction/rchudasa/MCGeneration'
